f0e295e403a599f1360478e10d9e9e62296be8dc
swissglider <npm@mailschweiz.com>
2022-04-23 13:32:55 +0200

    feat: init

--- bin/createReactComponentLib.js
+++ bin/createReactComponentLib.js
@@ -5,7 +5,6 @@ const { stderr } = require('process');
 const readline = require('readline');
 const packageJSON = require('./templates/toChange/package.json');
 const exec = require('child_process').exec;
-const { version } = require("./package.json");
 
 
 const successMSG = (msg => console.warn("\x1b[1m", "\x1b[32m", msg, "\x1b[0m"));
@@ -166,7 +165,8 @@ const main = async () => {
     // **************************************
     // Main Program
     // **************************************
-    successMSG(`Version ${version}`);
+    successMSG(`Version ${process.env.package_version}`);
+    console.log(process)
     await grapInputParameters();
     createProjectFolder();
     createPackageJSON();

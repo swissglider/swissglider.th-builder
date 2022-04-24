366b98e7a817738ccebd96c8d94953c9f977bf5d
swissglider <npm@mailschweiz.com>
2022-04-24 17:37:05 +0200

    feat: init

--- bin/messageFunctions.js
+++ bin/messageFunctions.js
@@ -2,9 +2,13 @@
 const successMSG = (msg => console.log("\x1b[1m", "\x1b[32m", msg, "\x1b[0m"));
 const warnMSG = (msg => console.warn("\x1b[2m", "\x1b[30m", "\x1b[43m", msg, "\x1b[0m"));
 // const errMSG = (msg => console.error("\x1b[5m", "\x1b[30m", "\x1b[41m", msg, "\x1b[0m"));
-const errMSG = (msg => console.error("\x1b[1m", "\x1b[31m", msg, "\x1b[0m"));
+const errMSG = (msg) => {
+    console.error("\x1b[1m", "\x1b[31m", msg, "\x1b[0m");
+}
 
-const waitMSG = (msg => console.log("\x1b[5m", "\x1b[34m", ` ☕ ☞ ', "\x1b[0m", "\x1b[1m", "\x1b[34m", msg, "\x1b[0m"));
+const waitMSG = (msg) => {
+     console.log("\x1b[5m", "\x1b[34m", " ☕ ☞ ", "\x1b[0m", "\x1b[1m", "\x1b[34m", msg, "\x1b[0m");
+}
 
 const successJobMSG = (msg, successIcon = "✔") => {
     process.stdout.moveCursor(0, -1) // up one line

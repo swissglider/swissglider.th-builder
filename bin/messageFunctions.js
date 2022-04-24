a4d3fa5d09be98bd953c5ec3795ffa195f3e77cd
swissglider <npm@mailschweiz.com>
2022-04-24 17:28:09 +0200

    feat: init

--- bin/messageFunctions.js
+++ bin/messageFunctions.js
@@ -3,17 +3,21 @@ const successMSG = (msg => console.log("\x1b[1m", "\x1b[32m", msg, "\x1b[0m"));
 const warnMSG = (msg => console.warn("\x1b[2m", "\x1b[30m", "\x1b[43m", msg, "\x1b[0m"));
 // const errMSG = (msg => console.error("\x1b[5m", "\x1b[30m", "\x1b[41m", msg, "\x1b[0m"));
 const errMSG = (msg => console.error("\x1b[1m", "\x1b[31m", msg, "\x1b[0m"));
-const waitMSG = (msg => console.log("\x1b[1m", "\x1b[34m", ` --> ${msg}`, "\x1b[0m"));
+
+const waitMSG = (msg => console.log("\x1b[5m", "\x1b[34m", ` ☕☞  ', "\x1b[1m", "\x1b[34m", '${msg}`, "\x1b[0m"));
+
 const successJobMSG = (msg, successIcon = "✔") => {
     process.stdout.moveCursor(0, -1) // up one line
     process.stdout.clearLine(1) // from cursor to end
     console.log("\x1b[1m", "\x1b[32m", ` ${successIcon}  `,"\x1b[0m" , msg);
 }
+
 const rewriteLastLine = (msg, writeFunction = successMSG) => {
     process.stdout.moveCursor(0, -1) // up one line
     process.stdout.clearLine(1) // from cursor to end
     writeFunction(msg);
 }
+
 const stdMSG = (msg => console.log(msg));
 
 const msgFunctions = {

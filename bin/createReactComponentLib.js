3577aa0013243b1f4e6b860690d45485c3f967ca
swissglider <npm@mailschweiz.com>
2022-04-24 15:45:59 +0200

    feat: init

--- bin/createReactComponentLib.js
+++ bin/createReactComponentLib.js
@@ -153,7 +153,8 @@ const adaptFilesWIthPackageName = async () => {
     const command2 = `sed -i "s/\${packageName}/${inputParams.packageName}/" ./liveStorybook/stories_/Default.stories.tsx`;
     console.log(command1);
     console.log(command2);
-    await execAsync(`ls -la`, cdw, {devNull:true});
+    const result = await execAsync(`ls -la`, cdw, {devNull:true});
+    console.log(result);
     await execAsync(`sed -i "s/\${packageName}/${inputParams.packageName}/" ./.github/workflows/gh-pages.yml`, cdw, {devNull:true});
     await execAsync(`sed -i "s/\${packageName}/${inputParams.packageName}/" ./liveStorybook/stories_/Default.stories.tsx`, cdw, {devNull:true});
     rewriteLastLine(' ✔  changed files with new packageName');
